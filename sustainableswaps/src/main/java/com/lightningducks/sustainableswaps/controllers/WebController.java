06e10d14677a10377555846a77de733f6557acb0
Camryn Sumabat <camrynsumabat@gmail.com>
2021-09-16 14:58:18 -0700

    A3 - Make individual HTTP call for A3 part 3

--- sustainableswaps/src/main/java/com/lightningducks/sustainableswaps/controllers/WebController.java
+++ sustainableswaps/src/main/java/com/lightningducks/sustainableswaps/controllers/WebController.java
@@ -1,7 +1,10 @@
 package com.lightningducks.sustainableswaps.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
@@ -9,6 +12,13 @@ public class WebController {
     @RequestMapping("/")
     public String getHome() {
         return "home.html";
+    }
+
+    @GetMapping(value = "/A3/camryn", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String A3Camryn() {
+        String text = "<html><body style='background-color: #D28F33;'><h1 style='text-align: center;'>My name is Camryn and I love pumpkin spice lattes.</h1></html></body>";
+        return text;
     }
 
 }
