8a9526650370b54f540d2d43c61c37a1b65fad31
Eric V <ercvldez@gmail.com>
2021-09-16 21:36:29 -0700

    Exercise 3 completed

--- sustainableswaps/src/main/java/com/lightningducks/sustainableswaps/controllers/WebController.java
+++ sustainableswaps/src/main/java/com/lightningducks/sustainableswaps/controllers/WebController.java
@@ -2,6 +2,8 @@ package com.lightningducks.sustainableswaps.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
@@ -9,6 +11,12 @@ public class WebController {
     @RequestMapping("/")
     public String getHome() {
         return "home.html";
+    }
+
+    @RequestMapping(value = "/sustainable/page", method = RequestMethod.GET)
+    @ResponseBody
+    String sustainablePage() {
+        return "A page of Sustainable Swaps";
     }
 
 }
