ff779a4cc88dbdfb5f347c7b314c5914ca8dd1fc
Eric V <ercvldez@gmail.com>
2021-09-17 18:48:01 -0700

    Exercise 2 completed

--- sustainableswaps/src/main/java/com/lightningducks/sustainableswaps/controllers/WebController.java
+++ sustainableswaps/src/main/java/com/lightningducks/sustainableswaps/controllers/WebController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
 
 @Controller
 public class WebController {
@@ -17,6 +20,14 @@ public class WebController {
     @ResponseBody
     String sustainablePage() {
         return "A page of Sustainable Swaps";
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    @ResponseBody
+    String jsoup() throws IOException{
+        Document jsoup = Jsoup.connect("http://jsoup.org").get();
+        String jsoupTitle = jsoup.title();
+        return (jsoupTitle);
     }
 
 }
