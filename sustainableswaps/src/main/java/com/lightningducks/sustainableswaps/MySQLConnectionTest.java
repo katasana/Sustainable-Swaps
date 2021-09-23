15d8f0512c96dd77f8b611ace5b4df1ed28221ce
Camryn Sumabat <camrynsumabat@gmail.com>
2021-09-23 11:08:00 -0700

    Add creation statement if database table does not exist yet

--- sustainableswaps/src/main/java/com/lightningducks/sustainableswaps/MySQLConnectionTest.java
+++ sustainableswaps/src/main/java/com/lightningducks/sustainableswaps/MySQLConnectionTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+
 @SpringBootApplication
 public class MySQLConnectionTest implements CommandLineRunner {
 
@@ -19,11 +20,26 @@ public class MySQLConnectionTest implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        // do an if-else for creating a new database if it doesn't already exist ?
-
         String sql = "INSERT INTO test (testId, testInfo) VALUES (?, ?)";
 
-        int result = jdbcTemplate.update(sql, "4", "test4");
+        // if the table 'test' already exists, insert a new row
+        try {
+            insertRow(sql);
+
+        } catch(Exception e) { // otherwise, create the table then add a new row
+
+            System.err.println("Table does not exist yet");
+            System.out.println("Creating table 'test'");
+
+            String createTable = "CREATE TABLE test.test (testId INT NOT NULL, testInfo VARCHAR(45) NOT NULL, PRIMARY KEY (testId))";
+            jdbcTemplate.execute(createTable);
+
+            insertRow(sql);
+        }
+    }
+
+    public void insertRow(String sql) {
+        int result = jdbcTemplate.update(sql, "5", "test5");
 
         if (result > 0) {
             System.out.println(result + "\nA new row has been inserted");
