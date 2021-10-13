2fb5b964854f9a4723be168b5a1cf4f34c788ed4
katasana <mlauw@cpp.edu>
2021-10-12 22:43:43 -0700

    A6 Megan

--- sustainableswaps/src/test/java/com/lightningducks/sustainableswaps/SustainableswapsApplicationTests.java
+++ sustainableswaps/src/test/java/com/lightningducks/sustainableswaps/SustainableswapsApplicationTests.java
@@ -1,5 +1,5 @@
 package com.lightningducks.sustainableswaps;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +8,13 @@ class SustainableswapsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testFindMinA6(){
+		SustainableswapsApplication a = new SustainableswapsApplication();
+		System.out.println("Find min test!");
+		Assertions.assertEquals(50, a.findMin(new int[]{5, 29, 10, 34}));
 	}
 
 }
