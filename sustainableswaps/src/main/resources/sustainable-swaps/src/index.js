e2d74597c87757f3a34c0e3f131415baa7679bb4
Eric V <ercvldez@gmail.com>
2021-10-20 10:14:57 -0700

    Created search results page

--- sustainableswaps/src/main/resources/sustainable-swaps/src/index.js
+++ sustainableswaps/src/main/resources/sustainable-swaps/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import SearchResults from './SearchResults';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <Router>
+        <Switch>
+            <Route path="/" exact> <Home/> </Route>
+            <Route path="/searchresults"> <SearchResults/> </Route>
+        </Switch>
+    </Router>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
